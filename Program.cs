68deef0d1c0bef8c3d40cb6bda67b26a345b2253
Dave Van den Eynde <dave@groovbird.com>
2017-02-06 12:14:39 +0100

    Initial commit.

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using AutoMapper;
+
+namespace ConsoleApp2
+{
+    public class Model
+    {
+        public ImmutableDictionary<string, string> Demo { get; set; }    
+    }
+
+    public class Dto
+    {
+        public Dictionary<string, string> Demo { get; set; }
+    }
+
+    public class ImmutableDictionaryConverter<TSource, TDestination> : ITypeConverter<ImmutableDictionary<TSource, TDestination>, IDictionary<TSource, TDestination>>
+    {
+        public IDictionary<TSource, TDestination> Convert(ImmutableDictionary<TSource, TDestination> source, IDictionary<TSource, TDestination> destination, ResolutionContext context)
+        {
+            return source?.ToDictionary(pair => pair.Key, pair => pair.Value);
+        }
+    }
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var configuration = new MapperConfiguration(configure =>
+            {
+                configure.CreateMap(typeof(ImmutableDictionary<,>), typeof(IDictionary<,>)).ConvertUsing(typeof(ImmutableDictionaryConverter<,>));
+                configure.CreateMap<Model, Dto>();
+            });
+
+            var mapper = configuration.CreateMapper();
+
+            var model = new Model()
+            {
+                Demo = ImmutableDictionary<string, string>.Empty.Add("Boo", "Yah")
+            };
+
+            var dto = mapper.Map<Dto>(model);
+
+            Console.WriteLine($"Boo: {dto.Demo["Boolean"]}");
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ConsoleApp2")]
+[assembly: AssemblyTrademark("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("79e3d413-78dd-47d3-8c29-a75968281936")]
